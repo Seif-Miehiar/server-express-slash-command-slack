4c20bfb865698e2006ccd7a789b406c6724bcc04
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-04-01 02:35:54 +0100

    updating-server

--- server/server.js
+++ server/server.js
@@ -2,32 +2,35 @@ const express = require("express");
 const app = express();
 var compression = require("compression");
 var bodyParser = require("body-parser");
-var path = require("path");
+// var path = require("path");
 
 let arrayOfLinks = [];
 
 const botUserOAuthAccessToken =
   "xoxb-958246117539-1025544532499-BmE8Kz7ALXFt9XnD5VVetDsK";
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(compression());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname + "../client/index.html"));
+// });
 
 app.post("/helloPost", (req, res) => {
   let linkObject = {};
+
   linkObject["userName"] = req.body.user_name;
   linkObject["text"] = req.body.text;
   linkObject["command"] = req.body.command;
   linkObject["user_id"] = req.body.user_id;
+
   arrayOfLinks.push(linkObject);
+
   console.log(linkObject, "\n", arrayOfLinks);
-  //   console.log(req.body);
+
   res.send("got a post request");
 });
 
