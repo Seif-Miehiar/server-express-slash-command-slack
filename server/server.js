9cf20243a17aabb667fc0183750cba06019ee342
hashem-buzer <hbuzer98@gmail.com>
2020-04-03 17:58:42 +0100

    last shot

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ let arrayOfLinks = [];
 const botUserOAuthAccessToken =
   "xoxb-958246117539-1025544532499-BmE8Kz7ALXFt9XnD5VVetDsK";
 
-app.use(express.static(__dirname + "/client/dist/client"));
+app.use(express.static(__dirname + "dist/client"));
 // console.log("PPPPPAAAAAATHHHHHHH", __dirname + "/client/dist/client");
 
 app.use(express.static("client"));
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(compression());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/dist/client/src"));
+  res.sendFile(path.join(__dirname + "dist/client/src"));
 });
 
 // function sendMessageToSlackResponseURL(JSONmessage) {
@@ -62,6 +62,6 @@ app.get("/all", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log(`listening to ${port}`);
 });
