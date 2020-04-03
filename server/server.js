d00ca39886578a008e7a565c4ceb1a203784d463
Seif-Miehiar <Seif.Miehiar@gmail.com>
2020-04-03 20:55:41 +0100

    testing

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 app.use(compression());
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "../dist/client/src/index.html"));
 });
 
