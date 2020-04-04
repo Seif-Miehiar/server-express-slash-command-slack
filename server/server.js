ae20a9ff3b19f06c72cb8dcf7c201d6007c12dc3
hashem-buzer <hbuzer98@gmail.com>
2020-04-04 11:06:11 +0100

    merged with seif master

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var cors = require("cors");
 
-let arrayOfLinks = [];
+let users = {};
 
 const botUserOAuthAccessToken =
   "xoxb-958246117539-1025544532499-BmE8Kz7ALXFt9XnD5VVetDsK";
 
-app.use(express.static(__dirname + "../dist/client"));
+app.use(express.static(__dirname + "/../client/dist/client"));
 
 app.use(express.static("client"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,23 +22,6 @@ app.use(compression());
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "../dist/client/src/index.html"));
 });
-
-// function sendMessageToSlackResponseURL(JSONmessage) {
-//   var postOptions = {
-//     uri:
-//       "https://hooks.slack.com/commands/TU6783FFV/1041997001798/IliHvlMXCb7N7gxNmpiM7j9e",
-//     method: "POST",
-//     headers: {
-//       "Content-type": "application/json"
-//     },
-//     json: {}
-//   };
-//   request(postOptions, (error, response, body) => {
-//     if (error) {
-//       // handle errors as you see fit
-//     }
-//   });
-// }
 
 app.post("/helloPost", (req, res) => {
   let linkObject = Object.assign(req.body);
