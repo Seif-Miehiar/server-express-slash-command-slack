1f0d4d52ccaec06006fd3f1181278b67c2ba59e6
hashem-buzer <hbuzer98@gmail.com>
2020-04-05 21:27:36 +0100

    design, delete item are done

--- client/src/app/components/one-student/one-student.component.ts
+++ client/src/app/components/one-student/one-student.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import Swal from 'sweetalert2';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-one-student',
@@ -7,11 +9,40 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class OneStudentComponent implements OnInit {
   @Input() student: any;
-  constructor() {}
+  constructor(private _http: HttpClient) {}
 
   ngOnInit() {}
 
   zoomLink() {
     window.open(this.student.zoomLink, '_blank');
+  }
+
+  deleteItem(id) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'error',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    })
+      .then((result) => {
+        if (result.value) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Deleted!!',
+            showConfirmButton: false,
+            timer: 1000,
+          });
+        }
+      })
+      .then(() =>
+        this._http.post(
+          `https://server-slash-command-slack.herokuapp.com/delete/${id}`,
+          {}
+        )
+      );
   }
 }
