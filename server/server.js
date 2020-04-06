58dffa373b876d10b1378afe7588e257ef0bbf3a
Mohamed-Dhia <56113399+mohamed-Dhia@users.noreply.github.com>
2020-04-06 21:22:30 +0100

    ficing small issues for integration

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var cors = require("cors");
 let users = {};
 const MAIN = new Main();
 const botUserOAuthAccessToken =
-  "xoxb-958246117539-1025544532499-BmE8Kz7ALXFt9XnD5VVetDsK";
+	"xoxb-958246117539-1025544532499-BmE8Kz7ALXFt9XnD5VVetDsK";
 
 app.use(express.static(__dirname + "/../client/dist/client"));
 
@@ -21,35 +21,37 @@ app.use(cors());
 app.use(compression());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "../dist/client/src/index.html"));
+	res.sendFile(path.join(__dirname + "../dist/client/src/index.html"));
 });
 
 app.get("/all", (req, res) => res.status(202).send(MAIN.students));
 
 app.post("/helloPost", (req, res) => {
-  console.log(req.body);
-  MAIN.addTable(req.body);
-  return res
-    .status(201)
-    .send("Thank you for sharing your Zoom link with us, HaPPy HaCkinG :D");
+	console.log(req.body);
+	MAIN.addTable(req.body);
+	return res
+		.status(201)
+		.send("Thank you for sharing your Zoom link with us, HaPPy HaCkinG :D");
 });
 
 app.delete("/delete/name/:name", (req, res) => {
-  MAIN.removeTable(req.params.user_name);
-  return res.status(202).send(MAIN.students);
-});
-app.delete("/delete/id/:id", (req, res) => {
-  MAIN.removeTable(req.params.id);
-  return res.status(202).send(MAIN.students);
+	MAIN.removeTableByName(req.params.user_name);
+	return res.status(202).send(MAIN.students);
 });
 
-app.delete("/delete/all", (req, res) => {
-  MAIN.emptyStudents();
-  res.status(202).send([]);
+app.delete("/delete/id/:id", (req, res) => {
+	console.log(req.params.id);
+	MAIN.removeTableById(req.params.id);
+	return res.status(202).send(MAIN.students);
+});
+
+app.delete("/deleteAll", (req, res) => {
+	MAIN.emptyStudents();
+	res.status(202).send([]);
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function () {
-  console.log(`☠☠ listening to ${PORT} ☠☠`);
+	console.log(`☠☠ listening to ${PORT} ☠☠`);
 });
